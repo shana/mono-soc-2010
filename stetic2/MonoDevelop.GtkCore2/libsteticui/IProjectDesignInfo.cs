0f2e7f355a4ca72dede1fc0b891a36872ab6a2a5
marecki.krzysztof <marecki.krzysztof@a470b8cb-0e6f-1642-1b45-71e107334c4b>
2010-06-01 08:24:28 +0000

    Stetic2 initial commit

---
+++ stetic2/MonoDevelop.GtkCore2/libsteticui/IProjectDesignInfo.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Stetic
+{
+	//Provides access to informations menaged by ide
+	public interface IProjectDesignInfo
+	{
+		//Returns component source file folder
+		string GetComponentFolder(string componentName);
+		
+		//Search for all components source file folders 
+		string[] GetComponentFolders ();
+	}
+}
+
