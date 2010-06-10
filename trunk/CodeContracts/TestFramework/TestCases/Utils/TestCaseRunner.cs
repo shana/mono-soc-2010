375beef1b4466c703c5d4943baec8b4bf0189476
chris@dunelm.org.uk <chris@dunelm.org.uk@a470b8cb-0e6f-1642-1b45-71e107334c4b>
2010-06-10 14:50:23 +0000

    Initial version of ccrewrite and cccheck test framework.

---
+++ trunk/CodeContracts/TestFramework/TestCases/Utils/TestCaseRunner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utils {
+    public static class TestCaseRunner {
+
+        public static void Run (Action testCase)
+        {
+            try {
+                testCase ();
+            } catch (Exception ex) {
+                Console.WriteLine (ex);
+            }
+        }
+
+    }
+}
