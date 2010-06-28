f970974682e504a4d3e836c31e2d335e6e6253ff
chris <chris@dunelm.org.uk>
2010-06-28 15:10:03 +0000

    The test-runner now successfully compares MS ccrewrite output with Mono ccrewrite output. Added a few more test cases.

---
+++ CodeContracts/TestFramework/TestCases/Inheritance_RequiresEqualsIntConst_Fail/Program.cs
@@ -0,0 +1,39 @@
+﻿#define CONTRACTS_FULL
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+using Utils;
+
+namespace Inheritance_RequiresEqualsIntConst_OK {
+
+	class A {
+
+		public virtual void Test (int i)
+		{
+			Contract.Requires (i == 5);
+		}
+
+	}
+
+	class B : A {
+
+		public override void Test (int i)
+		{
+			// Do nothing
+		}
+
+	}
+
+	class Program {
+		static void Main (string [] args)
+		{
+			TestCaseRunner.Run (() => {
+				B b = new B ();
+				b.Test (6);
+			});
+		}
+	}
+}
