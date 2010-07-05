76b98bec706852c7d94c7f92d2c1f2c036b32798
chris@dunelm.org.uk <chris@dunelm.org.uk@a470b8cb-0e6f-1642-1b45-71e107334c4b>
2010-07-05 14:55:38 +0000

    Updating test cases...

--- trunk/CodeContracts/TestFramework/TestCases/Utils/TestCaseRunner.cs
+++ trunk/CodeContracts/TestFramework/TestCases/Utils/TestCaseRunner.cs
@@ -6,13 +6,19 @@ using System.Text;
 namespace Utils {
     public static class TestCaseRunner {
 
-        public static void Run (Action testCase)
+        public static void Run (bool expectContractFailure, Action testCase)
         {
+			bool contractFailure = false;
             try {
                 testCase ();
             } catch (Exception ex) {
                 Console.WriteLine (ex);
+				contractFailure = true;
             }
+			if (expectContractFailure != contractFailure) {
+				//throw new InvalidOperationException ("Contract failure not consistent with expectations");
+				Console.WriteLine ("Unexpected contract behaviour - " + DateTime.UtcNow);
+			}
         }
 
     }
