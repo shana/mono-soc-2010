858e833e2698ca68cb6ebae54de49abe451fd197
lostfreeman <lostfreeman@a470b8cb-0e6f-1642-1b45-71e107334c4b>
2010-07-14 15:48:29 +0000

    * fixed compilation order panel height

--- branches/midterm/FSharpBinding/gtk-gui/MonoDevelop.FSharp.Project.CompilationOrderPanelWidget.cs
+++ branches/midterm/FSharpBinding/gtk-gui/MonoDevelop.FSharp.Project.CompilationOrderPanelWidget.cs
@@ -6,7 +6,7 @@ namespace MonoDevelop.FSharp.Project
 	{
 		private global::Gtk.VBox vbox1;
 
-		private global::Gtk.ScrolledWindow GtkScrolledWindow;
+		private global::Gtk.ScrolledWindow scrolledwindow2;
 
 		private global::Gtk.TreeView listProjectFiles;
 
@@ -19,21 +19,18 @@ namespace MonoDevelop.FSharp.Project
 			// Container child MonoDevelop.FSharp.Project.CompilationOrderPanelWidget.Gtk.Container+ContainerChild
 			this.vbox1 = new global::Gtk.VBox ();
 			this.vbox1.Name = "vbox1";
-			this.vbox1.Spacing = 12;
-			this.vbox1.BorderWidth = ((uint)(6));
 			// Container child vbox1.Gtk.Box+BoxChild
-			this.GtkScrolledWindow = new global::Gtk.ScrolledWindow ();
-			this.GtkScrolledWindow.Name = "GtkScrolledWindow";
-			this.GtkScrolledWindow.ShadowType = ((global::Gtk.ShadowType)(1));
-			// Container child GtkScrolledWindow.Gtk.Container+ContainerChild
+			this.scrolledwindow2 = new global::Gtk.ScrolledWindow ();
+			this.scrolledwindow2.Name = "scrolledwindow2";
+			this.scrolledwindow2.ShadowType = ((global::Gtk.ShadowType)(1));
+			// Container child scrolledwindow2.Gtk.Container+ContainerChild
 			this.listProjectFiles = new global::Gtk.TreeView ();
 			this.listProjectFiles.CanFocus = true;
 			this.listProjectFiles.Name = "listProjectFiles";
-			this.listProjectFiles.HeadersVisible = false;
 			this.listProjectFiles.Reorderable = true;
-			this.GtkScrolledWindow.Add (this.listProjectFiles);
-			this.vbox1.Add (this.GtkScrolledWindow);
-			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.GtkScrolledWindow]));
+			this.scrolledwindow2.Add (this.listProjectFiles);
+			this.vbox1.Add (this.scrolledwindow2);
+			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vbox1[this.scrolledwindow2]));
 			w2.Position = 0;
 			this.Add (this.vbox1);
 			if ((this.Child != null)) {
