2f602c4b289236908b280e83b879bf103a4581fb
chris <chris@dunelm.org.uk>
2010-07-19 13:31:54 +0000

    Removed old test framework. Added NUnit tests. rewriter can now read/write to/from streams as well as files. Fixed a few bugs.

---
+++ CodeContracts/Tools/ccrewriteTest/Program.cs
@@ -0,0 +1,26 @@
+﻿#define CONTRACTS_FULL
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+
+namespace ccrewriteTest {
+	class Program {
+		static void Main (string [] args)
+		{
+			using (var r = new RewriteAndLoad ()) {
+				r.Load ();
+				r.Call (() => Test1 (0));
+			}
+		}
+
+		static void Test1 (int i)
+		{
+
+			Contract.Requires (i == 4);
+		}
+
+	}
+}
