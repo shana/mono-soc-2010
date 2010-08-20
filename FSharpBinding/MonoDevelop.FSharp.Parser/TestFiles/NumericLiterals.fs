5adf06ba4f42a32c9916c7861fb7c17a73c82739
lostfreeman <lostfreeman@a470b8cb-0e6f-1642-1b45-71e107334c4b>
2010-08-20 17:06:25 +0000

    FSharpBinding: + F# parser which is able to produce AST for non-light F# syntax + build instructions + some additional info files about F# expressions

---
+++ FSharpBinding/MonoDevelop.FSharp.Parser/TestFiles/NumericLiterals.fs
@@ -0,0 +1,22 @@
+﻿#light "off"
+
+let (|Even|Odd|) x = if x % 2 = 0 then Even else Odd
+
+type StackItem = 
+    | I32
+    | I64
+    | Native
+    | Float
+    | Reference of int
+    | Object of int list
+
+type MyClass private () = class
+        let z = 10
+        member this.X = z
+        abstract member Y: int -> int
+        static member TT: int -> list<_> = fun x -> []
+    end
+
+let SBYTE = 0y;
+let BYTE = 0uy in
+for i :: tail in seq{for t in 1 .. 10 -> t * t} do printfn "%A" [] done
